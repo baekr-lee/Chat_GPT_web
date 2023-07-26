f923f4d3719abad8e42eff1eb47313fd2a6f059a
baekr-lee <133262655+baekr-lee@users.noreply.github.com>
2023-07-27 01:38:04 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+import streamlit as st
+
+st.title('Hello World!')
+
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import HumanMessage, SystemMessage
+
+OPENAI_API_KEY = "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"
+
+chat = ChatOpenAI(
+    temperature=0.7,
+    model_name="gpt-3.5-turbo",
+    openai_api_key=OPENAI_API_KEY
+)
+
+def send_click(chat,prompt):
+    messages = [SystemMessage(content='I want you to act as a philosopher'),HumanMessage(content=prompt)
+]
+    response = chat(messages).content
+    return response    
+
+def main():
+    st.title('Ask ChatGPT')
+    user_input = st.text_input("Question: ", key='prompt')
+    if st.button("Send"):
+        response = send_click(chat, user_input)
+        st.subheader("Answer: ")
+        st.success(response, icon= "👍")
+
+if  __name__ == '__main__':   
+    main()
